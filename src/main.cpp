a458b45b717d56f0285d381c1474ea52c670c723
DanielJ <danielwjameson@yahoo.com>
2017-10-14 16:20:58 -0500

    Correct test comments

--- src/main.cpp
+++ src/main.cpp
@@ -265,13 +265,13 @@ void equation_throws_tests(bool print) {
 
 	// Other
 	Test::check_equation_throws("(<3+2)", print);
-	Test::check_equation_throws("(!<3+2)", print); // NOTE: Unable to improve error reporting without integrating read_token into handle_token
+	Test::check_equation_throws("(!<3+2)", print);
 	Test::check_equation_throws("(3+2) * (/3)", print);
 	Test::check_equation_throws("(3-2)++", print);
 	Test::check_equation_throws("(3-2)++1", print); // TODO: Fix
 	Test::check_equation_throws("(3-2)!", print);
 	Test::check_equation_throws("3-!", print);
-	Test::check_equation_throws("3/(2!)", print); // NOTE: Unable to improve error reporting without integrating read_token into handle_token
+	Test::check_equation_throws("3/(2!)", print);
 	Test::check_equation_throws("2 + x", print);
 	Test::check_equation_throws("2 ? 3", print);
 	Test::check_equation_throws("3 - + 2", print);
